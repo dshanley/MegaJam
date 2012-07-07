7a59e60783dc73fa5bbe17ffaa26959e57ef6739
dave <dave@crowdcompass.com>
2012-07-07 10:52:52 -0700

    removed build folder

--- MegaJam/build/MegaJam.build/Debug-iphonesimulator/MegaJam.build/Objects-normal/i386/NXWViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/dshanley/Development/CrowdCompass/tmp/MegaJam/MegaJam/MegaJam/NXWViewController.m \
-  /Users/dshanley/Development/CrowdCompass/tmp/MegaJam/MegaJam/MegaJam/NXWViewController.h
