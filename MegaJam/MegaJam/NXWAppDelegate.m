5d5f7ef3fb5f0a826239c8e46ddfed2e50717fcf
Robert Corlett <robert.corlett@me.com>
2012-07-09 23:13:53 -0700

    Figuring out pulse effect and adding some basic UI elements

--- MegaJam/MegaJam/NXWAppDelegate.m
+++ MegaJam/MegaJam/NXWAppDelegate.m
@@ -10,6 +10,8 @@
 
 @implementation NXWAppDelegate
 
+@synthesize window = _window;
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
--- MegaJam/MegaJam/NXWViewController.m
+++ MegaJam/MegaJam/NXWViewController.m
@@ -9,21 +9,78 @@
 #import <AVFoundation/AVFoundation.h>
 #import <AVFoundation/AVAudioSession.h>
 #import "NXWViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
+#import <QuartzCore/QuartzCore.h>
+
 
 @interface NXWViewController ()
 
 @end
+
 
 @implementation NXWViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    UIButton *button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    [button setTitle:@"Play" forState:UIControlStateNormal];
-    [button addTarget:self action:@selector(playAudio) forControlEvents:UIControlEventTouchUpInside];
-    [button sizeToFit];
-    [self.view addSubview:button];
+    
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background@2x"]];
+    
+    //Play Button
+    UIButton *playButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    playButton.frame = CGRectMake(100, 140, 40, 20);
+    [playButton setTitle:@"Play" forState:UIControlStateNormal];
+    [playButton addTarget:self action:@selector(playAudio) forControlEvents:UIControlEventTouchUpInside];
+    [playButton sizeToFit];
+    
+    [self.view addSubview:playButton];
+    
+    //Pause Button
+    UIButton *pauseButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    pauseButton.frame = CGRectMake(160, 140, 40, 20);
+    [pauseButton setTitle:@"Pause" forState:UIControlStateNormal];
+    [pauseButton addTarget:self action:@selector(pauseAudio) forControlEvents:UIControlEventTouchUpInside];
+    [pauseButton sizeToFit];
+    
+    [self.view addSubview:pauseButton];
+    
+    //Volume Slider
+    CGRect frame = CGRectMake(40, 270, 240, 10);
+    UISlider *volumeSlider = [[UISlider alloc] initWithFrame:frame];
+    [volumeSlider addTarget:self action:@selector(adjustVolume:) forControlEvents:UIControlEventValueChanged];
+    [volumeSlider setBackgroundColor:[UIColor clearColor]];
+     volumeSlider.minimumValue = 0.0;
+     volumeSlider.maximumValue = 100.0;
+     volumeSlider.value = 25.0;
+    
+    [self.view addSubview:volumeSlider];
+    
+    //title
+    UIImageView *titleImage = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 100)];
+    titleImage.image = [UIImage imageNamed:@"title.png"];
+    [self.view addSubview:titleImage];
+    
+    
+    //image 1
+    UIImageView *imageViewBackground = [[UIImageView alloc] initWithFrame:CGRectMake(0, 320, 330, 285/2)];
+    imageViewBackground.image = [UIImage imageNamed:@"speaker1@2x"];
+    [self.view addSubview:imageViewBackground];
+    
+    //image 2
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 320, 330, 285/2)];
+    imageView.image = [UIImage imageNamed:@"speaker2@2x.png"];
+    
+    //setting up animation
+    CABasicAnimation *pulseAnimation;
+    pulseAnimation = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    pulseAnimation.duration = 1.0;
+    pulseAnimation.repeatCount = MAXFLOAT;
+    pulseAnimation.autoreverses = YES;
+    pulseAnimation.fromValue = [NSNumber numberWithFloat:1.0];
+    pulseAnimation.toValue = [NSNumber numberWithFloat:0.0];
+    [imageView.layer addAnimation:pulseAnimation forKey:@"animatePulse"];
+    
+    [self.view addSubview:imageView];
 }
 
 - (void)didReceiveMemoryWarning
@@ -84,6 +141,18 @@
     OSStatus err = AudioServicesCreateSystemSoundID ((__bridge CFURLRef)url,&soundFileObject);
     NSError *playError = [NSError errorWithDomain:NSOSStatusErrorDomain code:err userInfo:nil];
     AudioServicesPlaySystemSound (soundFileObject);     // should play into headset
+}
+
+- (void)pauseAudio {
+    //Pause Audio
+    NSLog(@"Pause Pressed...");
+}
+
+- (void)adjustVolume: (id)sender {
+    //Adjust Volume
+    UISlider *slider = (UISlider *) sender;
+    float value = slider.value;
+    NSLog(@"Slider value = %f", value);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
